850916ad9ced9e51df4b0cfb5ae176e68da520a7
Bohdana Kolesnyk <bogdana.kolesnyk@gmail.com>
2025-11-14 23:58:55 +0200

    Add EquipmentDtoV1 and EquipmentDtoV2 classes
    Introduce `EquipmentDtoV1` to represent basic equipment data
    with properties `Id`, `Name`, `InventoryNumber`, and `Status`.
    Add `EquipmentDtoV2` as an extension of `EquipmentDtoV1`
    with an additional `Details` property for new API support.
    Both classes are part of the `PstInventory.WebApp.Dtos`
    namespace.

---
+++ PstInventory.WebApp/Dtos/EquipmentDtoV1.cs
@@ -0,0 +1,9 @@
+﻿namespace PstInventory.WebApp.Dtos;
+
+public class EquipmentDtoV1
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? InventoryNumber { get; set; }
+    public string? Status { get; set; }
+}
---
+++ PstInventory.WebApp/Dtos/EquipmentDtoV2.cs
@@ -0,0 +1,7 @@
+﻿namespace PstInventory.WebApp.Dtos;
+
+public class EquipmentDtoV2 : EquipmentDtoV1
+{
+    // додаткове поле для нової версії API
+    public string? Details { get; set; }
+}
